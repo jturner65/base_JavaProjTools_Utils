d7e623e355f3eab8df5f379fcd3c6e30ab3e8dbd
John Turner <7strbass@gmail.com>
2022-11-09 13:18:18 -0500

    --Provide more rigorous string date/time output for myTimeMgr;

--- src/base_Utils_Objects/tools/myTimeMgr.java
+++ src/base_Utils_Objects/tools/myTimeMgr.java
@@ -1,29 +1,78 @@
 package base_Utils_Objects.tools;
 
 import java.time.*;
+import java.time.temporal.ChronoField;
 import java.time.format.*;
+import java.util.Locale;
 
 public class myTimeMgr {
 
 	/**
 	 * time of current process start, from initial construction of mapmgr - TODO use this to monitor specific process time elapsed.  set to 0 at beginning of a particular process, then measure time elapsed in process
 	 */
-	protected long curProcStartTime;
+	protected long curProcStartTime = 0;
 	/**
 	 * time prog initially run/built, in millis - used as offset for instant to provide smaller values for timestamp
 	 */
 	protected final long initialRunTime;
+	
 	/**
-	 * formatter for output
+	 * formatter for Date output
 	 */
-	DateTimeFormatter formatter;
-
+	private DateTimeFormatter preMadeDateFormatter;
+	private char curDateToken = '?';
+	/**
+	 * formatter for Time output
+	 */
+	private DateTimeFormatter preMadeTimeFormatter;
+	private char curTimeToken = '?';
+	
+	public myTimeMgr() {
+		this(Instant.now().toEpochMilli());
+	}
 	public myTimeMgr(long _initialRunTime) {
 		initialRunTime=_initialRunTime;
-		formatter = DateTimeFormatter.ofLocalizedDateTime( FormatStyle.SHORT).withZone( ZoneId.systemDefault() );
+		buildDateFormatter('-');
+		buildTimeFormatter(':');
+	}
+	
+	/**
+	 * Use this to specify a date formatter
+	 * @param dateToken
+	 */
+	public void buildDateFormatter(char dateToken) {
+		if(dateToken == curDateToken) {return;}
+		curDateToken = dateToken;
+		preMadeDateFormatter = new DateTimeFormatterBuilder()
+				.appendValue(ChronoField.YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
+                .appendLiteral(curDateToken)
+                .appendValue(ChronoField.MONTH_OF_YEAR, 2)
+                .appendLiteral(curDateToken)
+                .appendValue(ChronoField.DAY_OF_MONTH, 2)
+                .toFormatter(Locale.getDefault(Locale.Category.FORMAT)).withZone( ZoneId.systemDefault());		
+	}
+	
+	public void buildTimeFormatter(char timeToken) {
+		if(timeToken == curTimeToken) {return;}
+		curTimeToken = timeToken;
+		preMadeTimeFormatter = new DateTimeFormatterBuilder()
+                .appendValue(ChronoField.HOUR_OF_DAY, 2)
+                .appendLiteral(curTimeToken)
+                .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+                .optionalStart()
+                .appendLiteral(curTimeToken)
+                .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+                .optionalStart()
+                .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
+                .toFormatter(Locale.getDefault(Locale.Category.FORMAT)).withZone( ZoneId.systemDefault() );		
 	}
 	//////////////////////////
 	// time functions
+	/**
+	 * Returns time program was initialized/first run.
+	 * @return
+	 */
+	public long getInitRunTime() {return initialRunTime;}
 	/**
 	 * set time of current process start - all future time measurements will measure from here
 	 * @param _curProcStartTime
@@ -31,12 +80,12 @@ public class myTimeMgr {
 	public void setCurProcStartTime(long _curProcStartTime) {curProcStartTime=_curProcStartTime;}
 	
 	/**
-	 * get time from "start time" (instancing of owning object)
+	 * get time from "start time" (initialRunTime)
 	 * @return
 	 */
-	private long getCurTimeFromStart() {			
-		Instant instant = Instant.now();
-		return instant.toEpochMilli() - initialRunTime;//milliseconds since 1/1/1970, subtracting when mapmgr was built to keep millis low		
+	public long getCurTimeFromStart() {			
+		Instant now = Instant.now();
+		return now.toEpochMilli() - initialRunTime;//milliseconds since 1/1/1970, subtracting when mapmgr was built to keep millis low		
 	}//getCurTime() 
 	
 	/**
@@ -58,10 +107,45 @@ public class myTimeMgr {
 	}//getTimeStrFromPassedMillis	
 	
 	public String getCurrWallTime() {
-		String res = formatter.format(Instant.now());
+		String res = getDateTimeString();
 		return res;			
 	}
 	
+	/**
+	 * Build a date-time with given delimiter token
+	 * @return
+	 */
+	public String getDateTimeString(){
+		Instant now = Instant.now();
+		String result = preMadeDateFormatter.format(now);
+		result += "|";
+		result += preMadeTimeFormatter.format(now);
+		return result;
+	}
+	public String getDateTimeString(char dateToken, char timeToken){
+		Instant now = Instant.now();
+		String result = getDateString(now, dateToken);
+		result += getTimeString(now, timeToken);
+		return result;
+	}
+	//utilities
+	public String getDateString(){return getDateString(Instant.now(), '-');}
+	public String getDateString(char token){ return getDateString(Instant.now(), token);}
+	public String getDateString(Instant now, char token){
+		buildDateFormatter(token);
+		String result = preMadeDateFormatter.format(now);
+		return result;
+	}//getDateString
+	
+	public String getTimeString(){return getTimeString(Instant.now(), '-');}
+	public String getTimeString(char token){ return getTimeString(Instant.now(), token);}
+	public String getTimeString(Instant now, char token){
+		buildTimeFormatter(token);
+		String result  = preMadeTimeFormatter.format(now);
+		return result;
+	}//getDateString
+	
+
 	public String getWallTimeAndTimeFromStart(String delim) {	return getCurrWallTime() + delim + getTimeStrFromProcStart();}
 
 }//myTimeMgr
