48e89515a519e36af44be8fac91d291ed18103e2
John Turner <7strbass@gmail.com>
2023-02-24 12:50:59 -0500

    --add array -to-string utilities with format capability

--- src/base_Utils_Objects/tools/myTools.java
+++ src/base_Utils_Objects/tools/myTools.java
@@ -17,11 +17,73 @@ public class myTools {
      * @param vals
      * @return
      */
-    public synchronized static String digitAraToString(byte[] vals) {
+    public static String digitAraToString(byte[] vals) {
         StringBuilder b = new StringBuilder(vals.length);
         for (int i = 0;i< vals.length; i++) {b.append(vals[i]);}  	
         return b.toString();
     }
-     
+    /**
+     * Return a string holding the double values in the vals array of the form [(val0),(val1),...], using
+     * the passed format string 
+     * @param vals
+     * @param fmt
+     * @return
+     */
+    public static String arrayToString(double[] vals, String fmt) {
+    	StringBuilder b = new StringBuilder(vals.length );
+    	b.append("[");
+    	b.append(String.format(fmt, vals[0]));
+    	for(int i=1;i<vals.length;++i) { b.append(",");b.append(String.format(fmt, vals[i]));}
+    	b.append("]");
+    	return b.toString();
+    }
+ 
+    /**
+     * Return a string holding the float values in the vals array of the form [(val0),(val1),...], using
+     * the passed format string 
+     * @param vals
+     * @param fmt
+     * @return
+     */
+    public static String arrayToString(float[] vals, String fmt) {
+    	StringBuilder b = new StringBuilder(vals.length );
+    	b.append("[");
+    	b.append(String.format(fmt, vals[0]));
+    	for(int i=1;i<vals.length;++i) { b.append(",");b.append(String.format(fmt, vals[i]));}
+    	b.append("]");
+    	return b.toString();
+    }
+ 
+    /**
+     * Return a string holding the long values in the vals array of the form [(val0),(val1),...], using
+     * the passed format string 
+     * @param vals
+     * @param fmt
+     * @return
+     */
+    public static String arrayToString(long[] vals, String fmt) {
+    	StringBuilder b = new StringBuilder(vals.length );
+    	b.append("[");
+    	b.append(String.format(fmt, vals[0]));
+    	for(int i=1;i<vals.length;++i) { b.append(",");b.append(String.format(fmt, vals[i]));}
+    	b.append("]");
+    	return b.toString();
+    }
+
+    /**
+     * Return a string holding the int values in the vals array of the form [(val0),(val1),...], using
+     * the passed format string 
+     * @param vals
+     * @param fmt
+     * @return
+     */
+    public static String arrayToString(int[] vals, String fmt) {
+    	StringBuilder b = new StringBuilder(vals.length );
+    	b.append("[");
+    	b.append(String.format(fmt, vals[0]));
+    	for(int i=1;i<vals.length;++i) { b.append(",");b.append(String.format(fmt, vals[i]));}
+    	b.append("]");
+    	return b.toString();
+    }
 
 }//myTools
