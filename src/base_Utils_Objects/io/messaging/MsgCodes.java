10608c7332b43530f785075f2d623a54d582e2cc
john turner <7strbass@gmail.com>
2025-03-31 14:40:25 -0400

    --improve enums

--- src/base_Utils_Objects/io/messaging/MsgCodes.java
+++ src/base_Utils_Objects/io/messaging/MsgCodes.java
@@ -8,17 +8,16 @@ import java.util.*;
  *
  */
 public enum MsgCodes{
-	debug1(0),debug2(1),debug3(2), debug4(3), debug5(4),
-	info1(5),info2(6),info3(7), info4(8), info5(9),
-	warning1(10),warning2(11),warning3(12),warning4(13),warning5(14),	
-	error1(15),error2(16),error3(17),error4(18),error5(19);	
-	private int value; 
+	debug1, debug2, debug3, debug4, debug5,
+	info1, info2, info3, info4, info5,
+	warning1, warning2, warning3, warning4, warning5,	
+	error1, error2, error3, error4, error5;	
 	
 	private static Map<Integer, MsgCodes> map = new HashMap<Integer, MsgCodes>(); 
-	static { for (MsgCodes enumV : MsgCodes.values()) { map.put(enumV.value, enumV);}}
-	private MsgCodes(int _val){value = _val;} 
-	public int getVal(){return value;}
-	public static MsgCodes getVal(int idx){return map.get(idx);}
+	static { for (MsgCodes enumV : MsgCodes.values()) { map.put(enumV.ordinal(), enumV);}}
+	public int getVal(){return ordinal();}
+	public static MsgCodes getEnumByIndex(int idx){return map.get(idx);}
+	public static MsgCodes getEnumFromValue(int idx){return map.get(idx);}
 	public static int getNumVals(){return map.size();}						//get # of values in enum
 	@Override
     public String toString() { return ""+this.name(); }	
